ffec10b8e399a01107ec0d2e7fd2bd07e3374775
ArjunAtlast <arjunatlast@gmail.com>
2025-03-18 17:00:24 +0530

    chore: exported utility composables

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -2,3 +2,4 @@ export { default as FlowViewerRoot } from './FlowViewerRoot.vue';
 export { default as FlowViewerNode } from './FlowViewerNode.vue';
 export { default as FlowViewerConnection } from './FlowViewerConnection.vue';
 export { default as FlowViewerConnectionLabel } from './FlowViewerConnectionLabel.vue';
+export { useGridGuides, useFlowContext, useComputedLabelPosition, useComputedConnectionPathString } from './context';
